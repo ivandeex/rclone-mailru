dolfly/rclone

baidupcs: initial commit

backend/all/all.go
@@ -6,6 +6,7 @@ import (
 	_ "github.com/ncw/rclone/backend/amazonclouddrive"
 	_ "github.com/ncw/rclone/backend/azureblob"
 	_ "github.com/ncw/rclone/backend/b2"
+	_ "github.com/ncw/rclone/backend/baidupcs"
 	_ "github.com/ncw/rclone/backend/box"
 	_ "github.com/ncw/rclone/backend/cache"
 	_ "github.com/ncw/rclone/backend/crypt"

ivandeex/rclone-mailru

backend/all/all.go
@@ -6,6 +6,7 @@ import (
 	_ "github.com/rclone/rclone/backend/amazonclouddrive"
 	_ "github.com/rclone/rclone/backend/azureblob"
 	_ "github.com/rclone/rclone/backend/b2"
+	_ "github.com/rclone/rclone/backend/baidupcs"
 	_ "github.com/rclone/rclone/backend/box"
 	_ "github.com/rclone/rclone/backend/cache"
 	_ "github.com/rclone/rclone/backend/chunker"